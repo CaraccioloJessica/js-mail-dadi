355e370c0ffe64ddfccf2a9846ea5038cb5147d7
Jessica <jesscara.993@gmail.com>
2022-11-10 15:39:54 +0100

    if inside loop need fix

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,12 +1,20 @@
 // MAIL: Chiedi all’utente la sua email,controlla che sia nella lista di chi può accedere,stampa un messaggio appropriato sull’esito del controllo.
 
-// LISTA MAIL INVITATI
-const listaMail = ["luca@gmail.com", "marco@gmail.com", "paolo@gmail.com"];
-
 // MAIL INSERITA DA UTENTE
 let userMail = prompt("Inserisci la tua mail");
 console.log(userMail);
 
-// MESSAGGIO SE INVITATO
+// LISTA MAIL INVITATI
+const listaMail = ["luca@gmail.com", "marco@gmail.com", "paolo@gmail.com"];
 
-// MESSAGGIO SE NON INVITATO
+for (let i = 0; i < listaMail.length; i++){
+  // MESSAGGIO SEI INVITATO
+  if (userMail === listaMail[i]){
+    console.log("Sei tra gli invitati!")
+  }
+
+  // MESSAGGIO NON SE INVITATO
+  else{
+    console.log("Non sei tra gli invitati.")
+  }
+}
