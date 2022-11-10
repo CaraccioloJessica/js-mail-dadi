107bc223ffbe04b80f0a1797b9e78a67bd458860
Jessica <jesscara.993@gmail.com>
2022-11-10 15:18:55 +0100

    created mail list, verified prompt mail working

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,8 +1,11 @@
 // MAIL: Chiedi all’utente la sua email,controlla che sia nella lista di chi può accedere,stampa un messaggio appropriato sull’esito del controllo.
 
 // LISTA MAIL INVITATI
+const listaMail = ["luca@gmail.com", "marco@gmail.com", "paolo@gmail.com"];
 
 // MAIL INSERITA DA UTENTE
+let userMail = prompt("Inserisci la tua mail");
+console.log(userMail);
 
 // MESSAGGIO SE INVITATO
 
