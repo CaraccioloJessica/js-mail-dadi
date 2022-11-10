c8bbf2eb17c32d85cc95ca2855f080cf7ce1b5c4
Jessica <jesscara.993@gmail.com>
2022-11-10 17:13:31 +0100

    conditions established

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -1,7 +1,19 @@
 // Gioco dei dadi: Generare un numero random da 1 a 6 sia per il giocatore sia per il computer.Stabilire il vincitore, in base a chi fa il punteggio più alto. 
 
+// VARIABILI GIOCATORI
 const user = Math.floor(Math.random()* 6 + 1);
 const pc = Math.floor(Math.random()* 6 + 1);
 
 console.log("dado utente", user);
 console.log("dado computer",pc);
+
+// CONDIZIONE VINTO/PERSO/PAREGGIO
+if (user > pc){
+  console.log("Hai vinto tu!");
+}
+else if (user < pc){
+  console.log("Hai perso!")
+}
+else{
+  console.log("Pareggio!")
+}
