76b08266285cc0f801c8502ad1df22576ec340f7
Jessica <jesscara.993@gmail.com>
2022-11-10 15:11:54 +0100

    defined general structure and comments js

---
+++ mail/js/main.js
@@ -0,0 +1,9 @@
+// MAIL: Chiedi all’utente la sua email,controlla che sia nella lista di chi può accedere,stampa un messaggio appropriato sull’esito del controllo.
+
+// LISTA MAIL INVITATI
+
+// MAIL INSERITA DA UTENTE
+
+// MESSAGGIO SE INVITATO
+
+// MESSAGGIO SE NON INVITATO
